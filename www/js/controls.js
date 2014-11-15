0654521fc4856a2360053d74de7f4ab35de9148f
Benjamin Habegger <b@habegger.fr>
2014-11-15 22:30:01 +0100

    Added jumping

--- www/js/controls.js
+++ www/js/controls.js
@@ -1,18 +1,22 @@
 function move(obj, dx, dy) {
 	return function() {
 		var p = $(obj);
-		var transRegExp = /translate\((-?[0-9]+),(-?[0-9]+)\)/;
+		var transRegExp = /translate\((-?[0-9\.]+),(-?[0-9\.]+)\)/;
 		var trans = transRegExp.exec(p.attr("transform"));
 		var ox = Number(trans[1]);
 		var oy = Number(trans[2]);
 		var nx = ox + dx;
 		var ny = oy + dy;
+		if(ny >= 0) {
+			ny = 0;
+		}
+		console.log("translate("+nx+","+ny+")");
 		p.attr("transform","translate("+nx+","+ny+")")		
 	}
 }
 
 $(function(){
-	$('body').on('keyup',function(evt) {
+	$('body').on('keydown',function(evt) {
 		if(evt.keyCode == 37) {
 			$('.actions').trigger("left");
 		} else if(evt.keyCode == 38) {
@@ -22,15 +26,22 @@ $(function(){
 		} else if(evt.keyCode == 40) {
 			$('.actions').trigger("down");
 		}
-		return false;
-		console.log(evt.keyCode)
 	});
 		
 	
 	$('.actions').on("right",move("#player1",10,0));
 	$('.actions').on("left",move("#player1",-10,0));
 	$('.actions').on("up",function(){
-		$('#player1').trigger("jump");
+		$("#player1").animate({
+			dummy: 0,
+		},{
+			progress: function(a,p,left) {
+				var dx = 5;
+				var dy = -15*Math.cos(Math.PI * p);
+				console.log("p="+p+" dx="+dx+" dy="+dy);
+				move("#player1",dx,dy)();
+			}
+		})
 	});
 });
 
