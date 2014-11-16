03435537a5540e4af97565fc72689c025b84ddb3
Benjamin Habegger <b@habegger.fr>
2014-11-16 22:01:59 +0100

    Starting to work

--- www/js/controls.js
+++ www/js/controls.js
@@ -1,9 +1,6 @@
-function move(obj, dx, dy) {
-	return function() {
-		var p = $(obj);
-		
-	}
-}
+var leftPressed = false;
+var rightPressed = false;
+var upPressed = false;
 
 function update() {
 	var p = $("#player1");
@@ -21,11 +18,16 @@ function update() {
 		sumY += Number($(this).attr("y2"));
 	});
 
+	if(Math.abs(sumX) <= 1) sumX = 0;
+	if(Math.abs(sumY) <= 1) sumY = 0;
 	$('line.acceleration').attr("x2",sumX);
 	$('line.acceleration').attr("y2",sumY);
 
 	var speedX = Number($('line.speed').attr("x2")) + sumX;
 	var speedY = Number($('line.speed').attr("y2")) + sumY;
+	if(Math.abs(speedX) <= 10) speedX = 0;
+	if(Math.abs(speedY) <= 10) speedY = 0;
+	
 	$('line.speed').attr("x2",speedX);
 	$('line.speed').attr("y2",speedY);
 	
@@ -33,53 +35,58 @@ function update() {
 	var ny = oy + speedY;
 	
 	// Update forces for next round
+	$('g.forces line.horizontal').attr("x2", 0);
+	$('g.forces line.vertical').attr("y2", 0);
 	if(ny >= 550) {
 		// We're on the ground
 		ny = 550;
 		$('g.forces line.reaction').attr("y2", - Number($('g.forces line.gravity').attr("y2")));
+		$('g.forces line.friction').attr("y2", 0);
 		$('line.speed').attr("y2",0);
+		if(leftPressed && !rightPressed) {
+			$('g.forces line.horizontal').attr("x2",-30);	
+		}
+		if(!leftPressed && rightPressed) {
+			$('g.forces line.horizontal').attr("x2",30);	
+		}
+		if(upPressed) {
+			$('g.forces line.vertical').attr("y2",-150);
+		}
 	} else {
 		// We're in the air
 		$('g.forces line.reaction').attr("y2", 0);
-		$('g.forces line.horizontal').attr("x2", 0);
-		$('g.forces line.vertical').attr("y2", 0);
+		$('g.forces line.friction').attr("y2", 0.01 * (speedY * speedY) * (speedY > 0 ? -1 : 1));
 	}
 	$('g.forces line.friction').attr("x2", 0.01 * (speedX * speedX) * (speedX > 0 ? -1 : 1));
 	console.log("translate("+nx+","+ny+")");
 	p.attr("transform","translate("+nx+","+ny+")");
 }
 
-function walk(x,f) {
-	return function() {
-		$('g.forces line.horizontal').attr("x2",f);
-		update();
-	}
-}
-
-function jump(x,f) {
-	return function() {
-		$('g.forces line.vertical').attr("y2",f);
-		update();
-	}
-}
-
 $(function(){
 	$('body').on('keydown',function(evt) {
 		if(evt.keyCode == 37) {
-			$('.actions').trigger("left");
+			leftPressed = true;
 		} else if(evt.keyCode == 38) {
-			$('.actions').trigger("up");
+			upPressed = true;
 		} else if(evt.keyCode == 39) {
-			$('.actions').trigger("right");
+			rightPressed = true;
 		} else if(evt.keyCode == 40) {
-			$('.actions').trigger("down");
+			downPressed = true;
+		}
+	});
+	$('body').on('keyup',function(evt) {
+		if(evt.keyCode == 37) {
+			leftPressed = false;
+		} else if(evt.keyCode == 38) {
+			upPressed = false;
+		} else if(evt.keyCode == 39) {
+			rightPressed = false;
+		} else if(evt.keyCode == 40) {
+			downPressed = false;
 		}
 	});
 	
-	$('.actions').on("right",walk("#player1",30));
-	$('.actions').on("left",walk("#player1",-30));
-	$('.actions').on("up",jump("#player1",-60));
-	$('.actions').on("down",update);
+	setInterval(update,100);
 });
 
 
